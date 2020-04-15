0a79493fddca836384f052e0b5143983793509b2
robinrst <31628501+robinrst@users.noreply.github.com>
2020-04-15 18:06:03 +0530

    Add files via upload

---
+++ CountNoOFbinaryStrings.cpp
@@ -0,0 +1,99 @@
+#include        "algorithm"
+#include        "iostream"
+#include        "numeric"
+#include        "iomanip"
+#include        "cstring"
+#include        "math.h"
+#include        "bitset"
+#include        "string"
+#include        "vector"
+#include        "ctime"
+#include        "queue"
+#include        "stack"
+#include        "map"
+#include        "set"
+
+#include        "ext/pb_ds/assoc_container.hpp" // Common file
+#include        "ext/pb_ds/tree_policy.hpp" // Including tree_order_statistics_node_update
+#include        "ext/pb_ds/detail/standard_policies.hpp"
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define          f first
+#define          lgn 25
+#define          endl '\n'
+#define          sc second
+#define          pb push_back
+#define          N (int)2e5+5
+#define          PI acos(-1.0)
+#define          int long long 
+#define          vi vector<int>
+#define          mod 1000000007
+#define          ld long double
+#define          eb emplace_back
+#define          mii map<int,int>
+#define          vpii vector<pii>
+#define          pii pair<int,int>
+#define          pq priority_queue
+#define          BLOCK (int)sqrt(N)
+#define          test(x) while(x--)
+#define          all(x) begin(x),end(x)
+#define          allr(x) rbegin(x),rend(x)
+#define          fo(i,a,n) for(int i=a;i<n;i++)
+#define          rfo(i,n,a) for(int i=n;i>=a;i--)
+#define          FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define          time() cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n"
+#define 		 bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update > 
+OS ;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+     const char* comma = strchr (names + 1, ',');
+     cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int inf = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+
+int n,m,k,q;
+string s;
+vi adj[N];
+int dp[N][3]; // dp[i][0] -> ith index end at 0
+			// dp[i][1] -> ith index end at 1
+			// dp[i][2] -> number of valid binary strings end at ith index
+
+void go()
+{
+	cin >> n;
+
+	dp[1][0] = dp[1][1] = 1;
+
+	fo(i,2,n+1) 
+	{
+		dp[i][0] = dp[i-1][0] + dp[i-1][1];
+		dp[i][1] = dp[i-1][0];
+
+		dp[i][2] = dp[i][0] + dp[i][1];
+	}
+
+	cout << dp[n][2] << endl;
+
+
+}
+
+int32_t main()
+{
+	FAST;     
+	int t=1; 
+	cin>>t;
+	test(t) go();
+}
+
+ 
---
+++ Tilling-2.cpp
@@ -0,0 +1,97 @@
+#include        "algorithm"
+#include        "iostream"
+#include        "numeric"
+#include        "iomanip"
+#include        "cstring"
+#include        "math.h"
+#include        "bitset"
+#include        "string"
+#include        "vector"
+#include        "ctime"
+#include        "queue"
+#include        "stack"
+#include        "map"
+#include        "set"
+
+#include        "ext/pb_ds/assoc_container.hpp" // Common file
+#include        "ext/pb_ds/tree_policy.hpp" // Including tree_order_statistics_node_update
+#include        "ext/pb_ds/detail/standard_policies.hpp"
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define          f first
+#define          lgn 25
+#define          endl '\n'
+#define          sc second
+#define          pb push_back
+#define          N (int)2e5+5
+#define          PI acos(-1.0)
+#define          int long long 
+#define          vi vector<int>
+#define          mod 1000000007
+#define          ld long double
+#define          eb emplace_back
+#define          mii map<int,int>
+#define          vpii vector<pii>
+#define          pii pair<int,int>
+#define          pq priority_queue
+#define          BLOCK (int)sqrt(N)
+#define          test(x) while(x--)
+#define          all(x) begin(x),end(x)
+#define          allr(x) rbegin(x),rend(x)
+#define          fo(i,a,n) for(int i=a;i<n;i++)
+#define          rfo(i,n,a) for(int i=n;i>=a;i--)
+#define          FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define          time() cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n"
+#define 		 bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update > 
+OS ;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+     const char* comma = strchr (names + 1, ',');
+     cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int inf = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+
+int n,m,k,q;
+string s;
+
+void go()
+{
+	cin >> n >> m;
+	vi dp(n+m+1,0);
+
+	dp[1] = 1;
+	dp[m] = 2;
+
+	fo(i,2,n+1)
+	{
+		if( i <= m )
+		{
+			dp[i] = max( dp[i] , dp[i-1] );
+			dp[i] %= mod;
+		}
+		else dp[i] = (dp[i-1] + dp[i-m])%mod;
+	}
+	cout << dp[n]%mod << endl;
+
+}
+
+int32_t main()
+{
+	FAST;     
+	int t=1; 
+	cin>>t;
+	test(t) go();
+}
+
+ 
